b090450bb5a832c5c9396b0f651f516a51e6fc1f
Robert Ulmer <robert.ulmer@htlweiz.at>
2023-03-10 08:40:43 +0100

    Update criterion.sh

--- buildscripts/criterion.sh
+++ buildscripts/criterion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 criterion_target=$1
-cmake --build .. --target $(basename ${criterion_target})
+cmake --build . --target $(basename ${criterion_target})
 
 ./${criterion_target} || exit 2
